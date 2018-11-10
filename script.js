02a6b959e58c1e0ac0d4f0f7cfefe8ab29ac1360
Jon Fobert <jonrfobert@gmail.com>
2018-11-10 17:31:45 -0500

    add intro shapes and text

--- script.js
+++ script.js
@@ -2,28 +2,30 @@ const canvas = document.querySelector('#game');
 const context = canvas.getContext('2d');
 const canvasInstruct = document.querySelector('#instructions');
 const contextInstruct = canvasInstruct.getContext('2d');
+//consider getting rid of this to avoid float Baddie movement computation
 context.scale(10, 10);
 //Colors: https://coolors.co/3d5a80-98c1d9-e0fbfc-ee6c4d-293241
+
 
 const gravityAccelY = 70.0
 
 const baddieOne = {
 	x: 55,
-	y: 20,
+	y: 18,
 	width: 4,
 	height: 4
 };
 
 const baddieTwo = {
 	x: 200,
-	y: 20,
+	y: 18,
 	width: 4,
 	height: 4
 }
 
 const baddieThree = {
 	x: 230,
-	y: 20,
+	y: 18,
 	width: 4,
 	height: 4
 }
@@ -32,15 +34,23 @@ let baddies = [baddieOne, baddieTwo, baddieThree];
 
 let instructText = "UP arrow key to Jump"
 
+let introComplete = false
+
 function draw(deltaTime) {
 	context.fillStyle = '#E0FBFC';
 	context.fillRect(0, 0, canvas.width, canvas.height)
+	contextInstruct.clearRect(0,0,500,300)
 	contextInstruct.font = "24px Arial";
+	console.log(instructText);
 	contextInstruct.fillText(instructText, 120, 50);
 	if(player.velocityY !== 0) {
-		jump(deltaTime)
+		jump(deltaTime);
 	}
 	drawPlayer(player);
+	if (introComplete == false) {
+		introOnRails();
+		return;
+	}
 	baddies.forEach(baddie => {
 		drawBaddie(baddie);
 		baddie.x -= 0.2
@@ -56,8 +66,53 @@ function draw(deltaTime) {
 			updateScore();
 		}	
 	});
-
 }
+const introBaddie = {
+	x: 100,
+	y: 18,
+	width: 4,
+	height: 4
+};
+const introPad = {
+	x: 150,
+	y: 19,
+	width: 6,
+	height: 3
+};
+
+function introOnRails() {
+	stage = 
+	console.log(introBaddie.x)
+	drawBaddie(introBaddie);
+	drawPad(introPad);
+	introBaddie.x -= 0.1;
+	introPad.x -=0.1;
+	/*if(checkCollision(player,introBaddie)) {
+		player.score = 0;
+		updateScore();
+	}
+	if(checkCollision(player,introPad)){
+		player.score = 0;
+		updateScore();
+	}*/
+	if (introPad.x < -6) {
+		//contextInstruct.clearRect(0,0,500,300);
+		player.score++
+		updateScore();
+		return introComplete = true
+	}
+	else if (introBaddie.x < -4) {
+		instructText = "Jump on green pads for bonus points"
+		player.score++
+		updateScore();
+	}
+	else if (introBaddie.x < 50) {
+		//contextInstruct.clearRect(0,0,500,300);
+		instructText = "Jump to avoid red enemies";
+		//console.log(instructText)
+	}	
+};
+
 const player = {
 	x: 5,
 	y: 20,
@@ -72,9 +127,13 @@ function drawPlayer() {
 	context.fillRect(player.x, player.y, player.width, player.height)
 }
 
-
 function drawBaddie(baddie) {
 	context.fillStyle = '#EE6C4D';
+	context.fillRect(baddie.x, baddie.y, baddie.width, baddie.height);
+}
+
+function drawPad(baddie) {
+	context.fillStyle = '#99ED4B';
 	context.fillRect(baddie.x, baddie.y, baddie.width, baddie.height);
 }
 
